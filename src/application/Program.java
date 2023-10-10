1b7de6c8cfb38ede8ea648b4a39788d71950fefb
Arthur Coelho <arthurlevicoelho@hotmail.com>
2023-10-10 19:17:03 -0300

    Creation of the department

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departament departament = new Departament(1, "Iphone");
+		System.out.println(departament);
+	}
+
+}
