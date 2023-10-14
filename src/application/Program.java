a59486d9416a37c7fcb4ed0d8c8189a754cd4e19
Arthur Coelho <arthurlevicoelho@hotmail.com>
2023-10-14 17:24:54 -0300

    Interfaces Dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,14 @@ package application;
 
 import java.util.Date;
 
-import model.entities.Departament;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departament departament = new Departament(1, "Iphone");
+		Department departament = new Department(1, "Iphone");
 		
 		
 		Seller seller = new Seller(9,"Pedro","pedro@hotmail.com", new Date(),20000.0,departament);
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -14,12 +14,12 @@ public class Seller implements Serializable{
 	private Date birthDay;
 	private Double baseSalary;
 	
-	private Departament departmente;
+	private Department departmente;
 	
 	public Seller() {
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDay, Double baseSalary, Departament departmente) {
+	public Seller(Integer id, String name, String email, Date birthDay, Double baseSalary, Department departmente) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -68,11 +68,11 @@ public class Seller implements Serializable{
 		this.baseSalary = baseSalary;
 	}
 
-	public Departament getDepartmente() {
+	public Department getDepartmente() {
 		return departmente;
 	}
 
-	public void setDepartmente(Departament departmente) {
+	public void setDepartmente(Department departmente) {
 		this.departmente = departmente;
 	}
 
