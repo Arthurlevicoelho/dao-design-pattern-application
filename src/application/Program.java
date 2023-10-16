86d6745135252954ce5923733ff8d5a0a5251ef2
Arthur Coelho <arthurlevicoelho@hotmail.com>
2023-10-16 10:22:51 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department departament = new Department(1, "Iphone");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		
-		Seller seller = new Seller(9,"Pedro","pedro@hotmail.com", new Date(),20000.0,departament);
-		
-		//System.out.println(departament);
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -14,18 +14,18 @@ public class Seller implements Serializable{
 	private Date birthDay;
 	private Double baseSalary;
 	
-	private Department departmente;
+	private Department department;
 	
 	public Seller() {
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDay, Double baseSalary, Department departmente) {
+	public Seller(Integer id, String name, String email, Date birthDay, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDay = birthDay;
 		this.baseSalary = baseSalary;
-		this.departmente = departmente;
+		this.department = department;
 	}
 
 	public Integer getId() {
@@ -68,12 +68,12 @@ public class Seller implements Serializable{
 		this.baseSalary = baseSalary;
 	}
 
-	public Department getDepartmente() {
-		return departmente;
+	public Department getDepartment() {
+		return department;
 	}
 
-	public void setDepartmente(Department departmente) {
-		this.departmente = departmente;
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 
 	@Override
@@ -96,7 +96,7 @@ public class Seller implements Serializable{
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDay=" + birthDay + ", baseSalary="
-				+ baseSalary + ", departmente=" + departmente + "]";
+				+ baseSalary + ", department=" + department + "]";
 	}
 	
 	
