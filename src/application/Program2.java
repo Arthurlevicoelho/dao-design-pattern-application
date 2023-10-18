b217fd2c9fefeddde6cac1549aec8ea6d5bc4de2
Arthur Coelho <arthurlevicoelho@hotmail.com>
2023-10-18 16:42:19 -0300

    Insert

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -19,17 +19,23 @@ public class Program2 {
 			System.out.println(dep);
 		}
 		
-		System.out.println("\n=============TESTE ALLDEPARTMENT========\n");
+		System.out.println("\n=============TESTE FINDBYID========\n");
 		
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
 
-		System.out.println("\n=============TESTE ALLDEPARTMENT========\n");
+		System.out.println("\n=============TESTE UPDATE========\n");
 		
 		department = departmentDao.findById(1);
 		department.setName("Books");
 		departmentDao.update(department);
 		System.out.println("Update Completed!!!");
+		
+		System.out.println("\n=============TESTE INSERT========\n");
+		Department dep = new Department(null,"Computers");
+		departmentDao.insert(dep);
+		System.out.println("Insert completed!!!");
+		
 	}
 
 }
